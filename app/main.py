767e56fca10fa2cf381e1af13f5f02dc827625ec
Thomas Bowen <133tjb@gmail.com>
2023-04-11 23:41:06 -0700

    revise becuz socket recieve all bytes in one transmission

--- app/main.py
+++ app/main.py
@@ -3,11 +3,15 @@ import socket
 import re
 
 def extract_words(s):
-    matches = re.findall(r"\$\d+\r\n(.+?)\r\n", s)
+    print('extract')
+    matches = re.findall(rb"\$\d+\r\n(.+?)\r\n", s)
     return matches
 
 def is_ping(word):
     return word == b'ping'
+
+def is_RESP_array(data):
+    return b"*" in data
 
 
 def main():
@@ -19,30 +23,32 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept() # wait for client
 
-    data = b""
+    data = None
 
-    is_RESP_array = False
     array_size = None
     last_counted_amount_of_words = 0
 
 
     while True:
-        if is_RESP_array:
+        print("top of true ", is_RESP_array(data))
+
+        if data is not None and len(data) == 0:
+            data = None
+            continue
+
+        if data is not None and is_RESP_array(data):
+            print('enterrr')
             current_list_of_words = extract_words(data)
-            if len(current_list_of_words) > last_counted_amount_of_words:
-                for word in current_list_of_words[last_counted_amount_of_words:]:
-                    if is_ping(word):
-                        conn.send(b"+PONG\r\n")
-                    
-            last_counted_amount_of_words = len(current_list_of_words)
 
+            for word in current_list_of_words:
+                print(word)
+                if is_ping(word):
+                    conn.send(b"+PONG\r\n")
+            
+            data = None
 
-        if b"*" in data and not is_RESP_array:
-            is_RESP_array = True
-
-        if is_RESP_array and not array_size and data.index(b"*") < len(data) - 1:
-            array_size = data[data.index(b"*") + 1]
-
+        # if is_RESP_array and not array_size and data.index(b"*") < len(data) - 1:
+        #     array_size = data[data.index(b"*") + 1]
         
         chunk = conn.recv(256)
         print("chunk ", chunk)
